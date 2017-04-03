7e4655062e96180ca3c9a36ed88cafc11f9a85a9
Zack Campbell <zajrik@gmail.com>
2017-04-03 01:29:36 -0500

    Move git pull before npm install

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ server.post('/build/:id/:secret', (req, res) => {
 		if (branch === 'master')
 		{
 			console.log(`Starting docs build as of yamdbf/indev#${req.body.after}`);
-			result = execSync(`npm install && git pull && gulp && npm run docs:indev && cd ../yamdbf-docs && git commit -am "Build indev docs: ${req.body.after}" && git push`,
+			result = execSync(`git pull && npm install && gulp && npm run docs:indev && cd ../yamdbf-docs && git commit -am "Build indev docs: ${req.body.after}" && git push`,
 				{ cwd: config.indev }).toString();
 		}
 		else
 		{
 			console.log(`Starting docs build as of yamdbf/stable#${req.body.after}`);
-			result = execSync(`npm install && git pull && gulp && npm run docs:stable && cd ../yamdbf-docs && git commit -am "Build stable docs: ${req.body.after}" && git push`,
+			result = execSync(`git pull && npm install && gulp && npm run docs:stable && cd ../yamdbf-docs && git commit -am "Build stable docs: ${req.body.after}" && git push`,
 				{ cwd: config.stable }).toString();
 		}
 		console.log(result);
